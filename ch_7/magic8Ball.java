5a19b6e893fe5682cc1f7c0835b9afbe1d2cdc7d
myster-fantastik <natebemartin@gmail.com>
2018-03-04 16:30:09 +0800

    3/4
    magic8Ball

---
+++ ch_7/magic8Ball.java
@@ -0,0 +1,34 @@
+package ch_7;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class magic8Ball {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		Random r = new Random();
+		
+		String input;
+		
+		String[] list = {"Yes", "No", "Maybe", "Ask again later"};
+		
+		do {					
+			int randInt = r.nextInt(3);
+			
+			System.out.print("What's your Question?");
+			input = sc.nextLine();
+			System.out.println(list[randInt]);
+			r.nextInt();
+		} while (!input.matches("exit"));
+
+
+
+		
+		
+		
+			sc.close();
+	}
+
+}
