ed44ed4e057fa55e66b1706a83052f984f5aa4c5
myster-fantastik <natebemartin@gmail.com>
2018-03-07 13:58:00 +0800

    3/7 - password generator
    Code works as per requirements,
    but it is GARBAGE CODE

--- ch_7/passwordGenerator.java
+++ ch_7/passwordGenerator.java
@@ -1,6 +1,16 @@
+/*
+ * Create a program that generates a secure password. 
+ * Prompt the user for the minimum length, the number of special characters, 
+ * and the number of numbers. 
+ * 
+ * Then generate a password for the user using those inputs.
+ * 
+ * Use lists to store the characters you’ll use to generate the passwords.
+ * Add some randomness to the password generation.
+ */
+
 package ch_7;
 
-import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,27 +19,50 @@ public class passwordGenerator {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		Random rnd = new Random();
-				
-		int length;
+		//lists of strings where random values are pulled from
+		String upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String lowerChars = upperChars.toLowerCase();
+		String alphabet = upperChars + lowerChars;
+		String numbers = "0123456789";
+		String specChars = "[^\\d]";
 		
-		System.out.print("Enter a length:");
-		length = sc.nextInt();
+		//char[] length variables
+		int pwLength, numbersVar, specCharVar;
 		
-		String charCaps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		String chars = "abcdefghijklmnopqrstuvwxyz";
-		String nums = "0123456789";
-		String symbols = "!@#$%^&*()";
-		
-		String passSymbols = charCaps + chars + nums + symbols;
-		
-		char[] password = new char[length];
-		
-		for (int i = 0; i < length; i++) {
-			password[i] = passSymbols.charAt(rnd.nextInt(passSymbols.length()));
-		}
+		Random rand = new Random();
 
-		System.out.print(password);
+		//console input
+		System.out.print("Enter a minimum password length: ");
+		pwLength = sc.nextInt();		
+		System.out.print("Enter a numbers variable: ");
+		numbersVar = sc.nextInt();		
+		System.out.print("Enter a special characters variable: ");
+		specCharVar = sc.nextInt();		
+		
+		char password[] = new char[pwLength];
+		char numbersPW[] = new char[numbersVar];
+		char specCharPW[] = new char[specCharVar];			
+		
+		//program logic with three 'for' loops to generate random values from stored Strings
+		for (int i = 0; i < pwLength; i++) {
+			password[i] = alphabet.charAt(rand.nextInt(alphabet.length() - 1));
+		}
+		
+		for (int i = 0; i < numbersVar; i++) {
+			numbersPW[i] = numbers.charAt(rand.nextInt(numbers.length() - 1));
+		}
+		
+		for (int i = 0; i < specCharVar; i++) {
+			specCharPW[i] = specChars.charAt(rand.nextInt(specChars.length() - 1));
+		}
+		
+		//Concatenate all the char[] into one String 'masterPass'
+		String masterPass = (String.valueOf(password)) + (String.valueOf(numbersPW)) + (String.valueOf(specCharPW));
+		
+		//Generated password output
+		System.out.println(masterPass);
+		
+		sc.close();
 	}
 
 }
---
+++ sample.java
@@ -0,0 +1,7 @@
+
+public class sample {
+	  public static void main(String args[]) { 
+		int d = 0;    
+		int a = 42/d;
+	} 
+}
