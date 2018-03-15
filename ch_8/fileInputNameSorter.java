4193258f251c88b6a038bce679bcd5badd2c4882
myster-fantastik <natebemartin@gmail.com>
2018-03-15 16:06:10 +0800

    3/15
    Added comments to fileInputNameSorter &
    parseDataFile

--- ch_8/fileInputNameSorter.java
+++ ch_8/fileInputNameSorter.java
@@ -10,17 +10,23 @@ import java.util.Scanner;
 public class fileInputNameSorter {
 	
 	
-	public static void main(String[] args) throws IOException {		  
+	public static void main(String[] args) throws IOException {
+		  //File object used to read 'names.txt', wrapped in a Scanner
 		  Scanner s = new Scanner(new File("names.txt"));
 		  
+		  //List 'list' stores values from 'names.txt'
 		  List<String> list = new ArrayList<String>();
 		  
+		  //loops, adding 's' to 'list', chained to .useDelimeter()
+		  //splitting at ", "
 		  while (s.hasNext()){
 		      list.add(s.useDelimiter(", ").next());  
 		  }
 		  
+		  //'Collections.sort()', sorts 'list' alphabetically
 		  Collections.sort(list);
-
+		  
+		  //console output
 		  list.forEach(System.out::println);
 		  s.close();
 	}
--- ch_8/parseDataFile.java
+++ ch_8/parseDataFile.java
@@ -5,21 +5,25 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class parseDataFile {
 
 
 	public static void main(String[] args) throws FileNotFoundException {
+		//temporary String value
 		String fileInput;
-		
+		//List 'dataList' stores contents of 'dataFile.txt' as String values
 		List<String> dataList = new ArrayList<String>();
 		
-		
+		//try/catch block for 'dataFile.txt'
+		//FileReader reads contents of 'dataFile.txt', wrapped in a BufferedReader 'br'
 		try(BufferedReader br = new BufferedReader(new FileReader("dataFile.txt")))
 		
 		{
+			//loops until 'br' returns a null value
+			//'fileInput' is chained to 'replaceAll()',
+			//replacing "," with "   "
 			while((fileInput = br.readLine()) != null) {
 				dataList.add(fileInput.replaceAll(",", "  "));
 			}
@@ -27,6 +31,7 @@ public class parseDataFile {
 			System.out.println("I/O Error: " + exc);
 		}
 		
+		//console output
 		System.out.println("Last  First  Salary");
 		System.out.println("-------------------");
 		
